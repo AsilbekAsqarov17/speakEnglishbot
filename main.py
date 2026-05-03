dbbaec2be8bfd5b90124d6a9d11b527fe343747c
AsilbekAsqarov17 <asilbek.asqarov.2023@gmail.com>
2026-05-03 18:24:38 +0500

    initial deployement commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import logging
+import asyncio
+from unittest import result
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+from oxfordLookup import getDefinitions
+from deep_translator import GoogleTranslator
+from langdetect import detect
+API_TOKEN = '8534356157:AAEKq8NJcBqzJMKeOCcKVyH8EbjJmLGeh00'
+
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command('start'))
+async def send_welcome(message: types.Message):
+    await message.reply("Hi! I'm Translate bot by Voskar")
+
+@dp.message(Command('help'))
+async def send_help(message: types.Message):
+    await message.reply("This bot can give you defintions and pronunciations of the word you type and text translation!")
+
+@dp.message()
+async def tarjimon(message: types.Message):
+
+    if len(message.text.split()) > 2:
+
+        try:
+            translated_text = GoogleTranslator(source='auto', target='uz').translate(message.text)
+            if translated_text.strip().lower() == message.text.strip().lower():
+                translated_text = GoogleTranslator(source='auto', target='en').translate(message.text)
+
+            await message.reply(translated_text)
+        except Exception as e:
+            logging.error(f"Translation Error {e}")
+    else:
+        lang = detect(message.text)
+        if lang == 'en':
+            word_id = message.text
+        else:
+            word_id = GoogleTranslator(source='uz', target='en').translate(message.text)
+        lookup = getDefinitions(word_id)
+
+        if lookup:
+            await message.reply(f"Word: {word_id}\nDefinition: {lookup['definitions']}")
+            if lookup.get('audio'):
+                await message.reply_voice(lookup['audio'])
+        else:
+            await message.reply("Sorry, Could not find the word!")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Bot turned off")
---
+++ oxfordLookup.py
@@ -0,0 +1,33 @@
+import requests
+import json
+from pprint import pprint as print
+
+
+app_id = "0e513b44"
+app_key = "50ad99d39420b3c876da6270b2f296a6"
+language = "en-gb"
+
+def getDefinitions(word_id):
+    url = "https://od-api-sandbox.oxforddictionaries.com:443/api/v2/entries/" + language + "/" + word_id.lower()
+    r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+    res = r.json()
+    if 'error' in res.keys():
+        return False
+
+    output = {}
+    senses = res['results'][0]['lexicalEntries'][0]['entries'][0]['senses']
+
+    definitions = []
+    for sense in senses:
+        definitions.append(f"{sense['definitions'][0]}")
+
+    output['definitions'] = " ".join(definitions)
+
+    if res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0].get('audioFile'):
+        output['audio'] = res['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile']
+
+    return output
+
+if __name__ == "__main__":
+    print(getDefinitions('ace'))
+    print(getDefinitions('Great Britain'))
